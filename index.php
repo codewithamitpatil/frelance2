55449a1ade2db653298fefd9544b13a384f2a6c2
Amit Shrinivas gujar <62344675+codewithamitpatil@users.noreply.github.com>
2022-01-17 14:37:23 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -41,18 +41,19 @@
   <link rel="stylesheet" href="plugins/aos/aos.css">
 
   <!-- CUSTOM CSS -->
-  <link href="css2/style.css" rel="stylesheet">
+  <link href="css2/style2.css" rel="stylesheet">
   <link href="css/common.css" rel="stylesheet">
   <link href="css2/navigation.css" rel="stylesheet">
   <link href="css2/banner.css" rel="stylesheet">
   <link href="css2/footer.css" rel="stylesheet">
 
 
-
-  <link href="css2/blogsection.css" rel="stylesheet">
-  <link href="css2/contactUsSection.css" rel="stylesheet">
   <link href="css2/serviceSectionHome.css" rel="stylesheet" />
   <link rel="stylesheet" href="css2/productpage.css">
+
+  <link href="css2/blogsection.css" rel="stylesheet">
+  <link href="css2/contactUsMainSection.css" rel="stylesheet">
+
   <!-- <link href="css/aboutus.css" rel="stylesheet"> -->
 </head>
 
@@ -174,40 +175,40 @@
 
 
 
-    <section class="section  servicesSection">
+    <section class="servicesSection">
       <div class="container serviceWhite">
 
-        <p class="headerModify">Services</p>
+        <h2 class="headerModify">Services</h2>
         <div class="client-slider5">
-          <div class="item mb-4">
+          <div class="serviceCard ">
             <img class="m-auto" src="https://singaporelabs.sg/servicon/icon4.png" alt="business-finder">
             <h1>Mold Remediation</h1>
           </div>
-          <div class="item mb-4">
+          <div class=" serviceCard ">
             <img class="m-auto" src="https://singaporelabs.sg/servicon/icon5.png" alt="forbes">
             <h1>IHMS</h1>
           </div>
-          <div class="item mb-4">
+          <div class=" serviceCard ">
             <img class="m-auto" src="https://singaporelabs.sg/servicon/icon1.png" alt="venture-beat">
             <h1>Incubation Center</h1>
           </div>
-          <div class="item mb-4">
+          <div class=" serviceCard ">
             <img class="m-auto" src="https://singaporelabs.sg/servicon/icon2.png" alt="TechCrunch">
             <h1>IAQ Consulting</h1>
           </div>
-          <div class="item mb-4">
+          <div class="serviceCard ">
             <img class="m-auto" src="https://singaporelabs.sg/servicon/icon4.png" alt="business-finder">
             <h1>Mold Remediation</h1>
           </div>
-          <div class="item mb-4">
+          <div class=" serviceCard ">
             <img class="m-auto" src="https://singaporelabs.sg/servicon/icon5.png" alt="forbes">
             <h1>IHMS</h1>
           </div>
-          <div class="item mb-4">
+          <div class="serviceCard ">
             <img class="m-auto" src="https://singaporelabs.sg/servicon/icon1.png" alt="venture-beat">
             <h1>Incubation Center</h1>
           </div>
-          <div class="item mb-4">
+          <div class="serviceCard">
             <img class="m-auto" src="https://singaporelabs.sg/servicon/icon2.png" alt="TechCrunch">
             <h1>IAQ Consulting</h1>
           </div>
@@ -219,7 +220,7 @@
 
 
 
-    <section class="section clients bg-gray productsSection">
+    <section class="productsSection">
       <div class="container">
 
         <div class="row">
@@ -260,62 +261,31 @@
     <!--======================================
 =            Related Article            =
 =======================================-->
-    <section class="section blogSection related-articles bg-gray">
+    <section class=" blogSection  ">
       <div class="container">
         <div class="row">
-          <div class="col-12 title ">
+          <div class="col-md-12 col-sm-12 col-lg-12">
             <h2 class="headerModify">Blogs</h2>
           </div>
+
         </div>
         <div class=" row blog-slider1">
 
 
-
-
           <div class="col-lg-4 col-md-6">
             <!-- Post -->
-            <article class="post-sm">
+            <article class="blog-sm">
               <!-- Post Image -->
-              <div class="post-thumb">
+              <div class="blog-thumb">
                 <a href="blog-single.html"><img class="w-100" src="images/blog/post-02.jpg" alt="Post-Image"></a>
               </div>
               <!-- Post Title -->
-              <div class="post-title">
+              <div class="blog-title">
                 <h3><a href="blog-single.html">Design is not just what it looks like and feels like. Design is how it
                     works.</a></h3>
               </div>
               <!-- Post Meta -->
-              <div class="post-meta">
-                <ul class="list-inline post-tag">
-                  <li class="list-inline-item">
-                    <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="author-thumb">
-                  </li>
-                  <li class="list-inline-item">
-                    <a href="#">Thomas Johnson</a>
-                  </li>
-                  <li class="list-inline-item">
-                    August 8, 2017
-                  </li>
-                </ul>
-              </div>
-              <!-- Post Details -->
-
-            </article>
-          </div>
-          <div class="col-lg-4 col-md-6">
-            <!-- Post -->
-            <article class="post-sm">
-              <!-- Post Image -->
-              <div class="post-thumb">
-                <a href="blog-single.html"><img class="w-100" src="images/blog/post-02.jpg" alt="Post-Image"></a>
-              </div>
-              <!-- Post Title -->
-              <div class="post-title">
-                <h3><a href="blog-single.html">Design is not just what it looks like and feels like. Design is how it
-                    works.</a></h3>
-              </div>
-              <!-- Post Meta -->
-              <div class="post-meta">
+              <div class="blog-meta">
                 <ul class="list-inline post-tag">
                   <li class="list-inline-item">
                     <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="author-thumb">
@@ -335,18 +305,18 @@
 
           <div class="col-lg-4 col-md-6">
             <!-- Post -->
-            <article class="post-sm">
+            <article class="blog-sm">
               <!-- Post Image -->
-              <div class="post-thumb">
+              <div class="blog-thumb">
                 <a href="blog-single.html"><img class="w-100" src="images/blog/post-02.jpg" alt="Post-Image"></a>
               </div>
               <!-- Post Title -->
-              <div class="post-title">
+              <div class="blog-title">
                 <h3><a href="blog-single.html">Design is not just what it looks like and feels like. Design is how it
                     works.</a></h3>
               </div>
               <!-- Post Meta -->
-              <div class="post-meta">
+              <div class="blog-meta">
                 <ul class="list-inline post-tag">
                   <li class="list-inline-item">
                     <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="author-thumb">
@@ -365,18 +335,18 @@
           </div>
           <div class="col-lg-4 col-md-6">
             <!-- Post -->
-            <article class="post-sm">
+            <article class="blog-sm">
               <!-- Post Image -->
-              <div class="post-thumb">
+              <div class="blog-thumb">
                 <a href="blog-single.html"><img class="w-100" src="images/blog/post-02.jpg" alt="Post-Image"></a>
               </div>
               <!-- Post Title -->
-              <div class="post-title">
+              <div class="blog-title">
                 <h3><a href="blog-single.html">Design is not just what it looks like and feels like. Design is how it
                     works.</a></h3>
               </div>
               <!-- Post Meta -->
-              <div class="post-meta">
+              <div class="blog-meta">
                 <ul class="list-inline post-tag">
                   <li class="list-inline-item">
                     <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="author-thumb">
@@ -393,6 +363,38 @@
 
             </article>
           </div>
+
+          <div class="col-lg-4 col-md-6">
+            <!-- Post -->
+            <article class="blog-sm">
+              <!-- Post Image -->
+              <div class="blog-thumb">
+                <a href="blog-single.html"><img class="w-100" src="images/blog/post-02.jpg" alt="Post-Image"></a>
+              </div>
+              <!-- Post Title -->
+              <div class="blog-title">
+                <h3><a href="blog-single.html">Design is not just what it looks like and feels like. Design is how it
+                    works.</a></h3>
+              </div>
+              <!-- Post Meta -->
+              <div class="blog-meta">
+                <ul class="list-inline post-tag">
+                  <li class="list-inline-item">
+                    <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="author-thumb">
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">Thomas Johnson</a>
+                  </li>
+                  <li class="list-inline-item">
+                    August 8, 2017
+                  </li>
+                </ul>
+              </div>
+              <!-- Post Details -->
+
+            </article>
+          </div>
+
         </div>
 
       </div>
@@ -401,9 +403,9 @@
 
 
 
-    <section class="section contactUsSection">
+    <div class="contactUsSection">
 
-      <div class="contact3 py-5">
+      <div class="contact3 ">
         <div class="row no-gutters">
           <div class="container">
             <div class="row picUpContact">
@@ -440,7 +442,7 @@
               </div>
               <div class="col-lg-12">
                 <div class="card mt-4 border-0 mb-4">
-                  <div class="row  bgWhite">
+                  <div class="row  bgWhite2">
 
                     <div class="col-lg-4 col-md-4">
                       <div class="card-body d-flex align-items-center c-detail">
@@ -490,7 +492,7 @@
         </div>
       </div>
 
-    </section>
+    </div>
 
 
     <!--============================
